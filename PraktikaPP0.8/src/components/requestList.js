2d250aeffc736734ea7b7b988f4d9da1974d21b9
MakeyK <zevstvsrgk@gmail.com>
2024-12-19 09:46:03 +0300

    Начало создания листа

--- PraktikaPP0.8/src/components/requestList.js
+++ PraktikaPP0.8/src/components/requestList.js
@@ -1,30 +1,48 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import { observer } from "mobx-react-lite";
-import { Col } from "react-bootstrap";
+import { Col, ListGroupItem } from "react-bootstrap";
 
 
 const RequestList = observer(({ user }) => {
     return (
         <ListGroup style={{
-            display: "inline-block",
+            display:'inline-block', marginTop:'25%',
             borderRadius: "0 px",
             marginLeft: 15, color: '#FFFFFF4D',
-            fontSize: 100
+            fontSize: '48px', fontFamily: 'SeoulHangang CEB'
         }}>
             {
                 user.map((data) => (
-                    <ListGroup.Item key={data.id_request} style={{ backgroundColor: '#FFFFFF4D' }}>
-                        <Col style={{
-                            border: "1px solid black",
-                            borderRadius: "10px", marginTop: "100px", overflow: 'scroll'
+                    <ListGroup key={data.id_request} style={{ backgroundColor: '#878787', display:''}} className="my-2">
+                        {/* <Col style={{
+                            display:'flex', justifyContent:'center', alignItems:'center',
+                            border: "1px solid #878787",
+                            borderRadius: "50px", overflow: 'scroll'
                         }}>
-                            {data.status_request == "Новое" ? <div style={{ color: "blue" }}>Новое</div> : data.status_request == "Отклонено" ? <div
+                            {data.status_request == "Новое" ?
+                             <div style={{ color: "blue" }}>Новое</div> : data.status_request == "Отклонено" ? <div
                                 style={{ color: "red" }}>Отклонено</div> :
-                                <div style={{ color: "green" }}>Подтверждено</div>}
+                                <div style={{ color: "green" }}>Подтверждено</div>
                             <div style={{ color: "black" }}>Описание: <div>{data.description_problem}</div>
                             </div>
-                        </Col>
-                    </ListGroup.Item>
+                        </Col> */}
+                        <ListGroup.Item style={{fontSize:'48px', background:'#878787', color:'white', letterSpacing: '8.16px', fontFamily: 'SeoulHangang CEB', width:'1500px'}}>
+                                №
+                        </ListGroup.Item>
+                        <ListGroup.Item style={{fontSize:'48px', background:'#878787', color:'white', letterSpacing: '8.16px', fontFamily: 'SeoulHangang CEB'}}>
+                            {data.tema}
+                        </ListGroup.Item>
+                        <ListGroup.Item style={{fontSize:'48px', background:'#878787', color:'white', letterSpacing: '8.16px', fontFamily: 'SeoulHangang CEB'}}>
+                            {data.description_problem}
+                        </ListGroup.Item>
+                        <ListGroup.Item style={{fontSize:'48px', background:'#878787', color:'white', letterSpacing: '8.16px', fontFamily: 'SeoulHangang CEB'}}>
+                            {data.date_and_time_supply}
+                        </ListGroup.Item>
+                        <ListGroup.Item style={{fontSize:'48px', background:'#878787', color:'white', letterSpacing: '8.16px', fontFamily: 'SeoulHangang CEB'}}>
+                        
+                        </ListGroup.Item>
+
+                    </ListGroup>
                 ))
             }
         </ListGroup>
